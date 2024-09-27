5fd83c9c7578a6b26cecaf5aed80790e6dce9e24
jlonge421 <jlonge421@gmail.com>
2024-09-27 15:34:02 -0400

    first commit

---
+++ client.py
@@ -0,0 +1,2 @@
+# This will reside on the clients machine,
+# allowing them to communicate with the server-side script
---
+++ server.py
@@ -0,0 +1,2 @@
+# This script will be server-side,
+# conducting math calculations
